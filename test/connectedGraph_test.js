ef3ce3156d2ccf1ad7b518284456ae4f4acb75a1
Max Antinone <mantinone@gmail.com>
2017-01-25 16:11:58 -0800

    Add more tests

--- test/connectedGraph_test.js
+++ test/connectedGraph_test.js
@@ -20,10 +20,31 @@ describe('connectedGraph()', function(){
       'c': ['d'],
       'd': ['c']
     }
+    const graphC = {
+      'a': ['b','c'],
+      'b': ['a'],
+      'c': ['a'],
+      'd': ['e'],
+      'e': ['d'],
+    }
+    const graphD = {
+      'a': []
+    }
+    const graphE = {
+      'a': [],
+      'b': []
+    }
 
     let a = connectedGraph(graphA)
     let b = connectedGraph(graphB)
+    let c = connectedGraph(graphC)
+    let d = connectedGraph(graphD)
+    let e = connectedGraph(graphE)
+    
     expect(a).to.equal(true)
     expect(b).to.equal(false)
+    expect(c).to.be.false
+    expect(d).to.be.true
+    expect(e).to.be.false
   })
 })
