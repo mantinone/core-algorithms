11944e00a847c0e5c55005f646f3507a95af240a
Serafin <leaf466@gmail.com>
2017-01-27 12:17:48 -0800

    Remove unneeded comments

--- extension/background.js
+++ extension/background.js
@@ -4,13 +4,7 @@ var optionValue = "aaa"
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.request === 'feedMe'){
     sendResponse({option: optionValue})
-  }
-  if(request.request === 'setMe'){
+  } else if(request.request === 'setMe'){
     optionValue = request.option
-
   }
-  // chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-  //
-  //   //chrome.tabs.sendMessage(tabs[0].id, request.option)
-  // })
-});
+})
